6d7ac8d5dfabf3dd5d49cd62468ad7b479a589a7
omrobbie <putu.roby@yahoo.com>
2017-05-03 11:33:31 +0800

    tampilkan score

---
+++ Assets/Scripts/ShowScore.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowScore : MonoBehaviour
+{
+    public Text txtScore;
+
+    private void FixedUpdate()
+    {
+        txtScore.text = Data.Score.ToString();
+    }
+}
