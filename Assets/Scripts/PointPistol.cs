5ff9e15f2fbe7bd15e174b5c7044bb44c8d00c2a
omrobbie <putu.roby@yahoo.com>
2017-05-03 09:11:27 +0800

    tambahkan pistol, sudah bisa menembakkan peluru

---
+++ Assets/Scripts/PointPistol.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PointPistol : MonoBehaviour
+{
+    public Rigidbody bullet;
+    [Range(0, 100)]
+    public float speed = 30;
+
+    private void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Rigidbody newBullet = GameObject.Instantiate(bullet, transform.position, transform.rotation);
+            newBullet.velocity = transform.TransformDirection(Vector3.forward * speed);
+            Physics.IgnoreCollision(newBullet.GetComponent<SphereCollider>(), transform.parent.GetComponent<BoxCollider>(), true);
+        }
+    }
+}
