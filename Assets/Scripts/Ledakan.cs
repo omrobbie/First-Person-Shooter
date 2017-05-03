2736ad900dd37577f539f31ee67e50ad5bc695fd
omrobbie <putu.roby@yahoo.com>
2017-05-03 08:47:10 +0800

    bikin peluru dan jadikan prefabs

---
+++ Assets/Scripts/Ledakan.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Ledakan : MonoBehaviour
+{
+    public GameObject particleLedakan;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint cp = collision.contacts[0];
+        Quaternion rotasi = Quaternion.FromToRotation(Vector3.up, cp.normal);
+        GameObject.Instantiate(particleLedakan, cp.point, rotasi);
+        Destroy(gameObject);
+    }
+}
